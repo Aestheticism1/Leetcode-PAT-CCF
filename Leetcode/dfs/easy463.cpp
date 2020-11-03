1e57e813230d5d2fc593a1ef05c6b0017b162d47
Aestheticism1 <250202498@qq.com>
2020-11-03 10:04:20 +0800

    dfs

--- Leetcode/dfs/easy463.cpp
+++ Leetcode/dfs/easy463.cpp
@@ -21,4 +21,35 @@ public:
         }
         return count;
     }
+
+    int dfs(int x, int y, vector<vector<int>> &grid, int n, int m) {
+        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) {
+            return 1;
+        }
+        if (grid[x][y] == 2) {
+            return 0;
+        }
+        grid[x][y] = 2;
+        int res = 0;
+        for (int i = 0; i < 4; ++i) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            res += dfs(tx, ty, grid, n, m);
+        }
+        return res;
+    }
+
+    int islandPerimeter_dfs(vector<vector<int>> &grid) {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    ans += dfs(i, j, grid, n, m);
+                }
+            }
+        }
+        return ans;
+    }
+
 };
