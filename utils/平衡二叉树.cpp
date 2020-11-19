2deca859f8ae5174437f9f6e49da9cc0ab92ed3f
Aestheticism1 <250202498@qq.com>
2020-11-19 09:45:27 +0800

    AVL_tree

--- utils/平衡二叉树.cpp
+++ utils/平衡二叉树.cpp
@@ -12,39 +12,39 @@ int getHeight(AVLTreeNode *root){
     return root == nullptr ? 0 : root->height;
 }
 
-AVLTreeNode* LLRotation(AVLTreeNode* &k2){
+AVLTreeNode* LLRotation(AVLTreeNode* k2){
     AVLTreeNode* k1 = k2->left;
     k2->left = k1->right;
     k1->right = k2;
-    k2->height = max(k2->left->height, k2->right->height) + 1;
-    k1->height = max(k1->left->height, k2->height) + 1;
+    k2->height = max(getHeight(k2->left), getHeight(k2->right)) + 1;
+    k1->height = max(getHeight(k1->left), k2->height) + 1;
     return k1;
 }
 
-AVLTreeNode* RRRotation(AVLTreeNode* &k1){
+AVLTreeNode* RRRotation(AVLTreeNode* k1){
     AVLTreeNode* k2 = k1->right;
     k1->right = k2->left;
     k2->left = k1;
-    k1->height = max(k1->left->height, k1->right->height) + 1;
-    k2->height = max(k1->height, k2->right->height) + 1;
+    k1->height = max(getHeight(k1->left), getHeight(k1->right)) + 1;
+    k2->height = max(k1->height, getHeight(k2->right)) + 1;
     return k2;
 }
 
-AVLTreeNode* LRRotation(AVLTreeNode* &k){
+AVLTreeNode* LRRotation(AVLTreeNode* k){
     k->left = RRRotation(k->left);
     return LLRotation(k);
 }
 
-AVLTreeNode* RLRotation(AVLTreeNode* &k){
+AVLTreeNode* RLRotation(AVLTreeNode* k){
     k->right = LLRotation(k->right);
     return RRRotation(k);
 }
 
-AVLTreeNode* insert(AVLTreeNode* &root, int data){
+AVLTreeNode* Insert(AVLTreeNode* &root, int data){
     if(root == nullptr){
-        return new AVLTreeNode(data);
+        root = new AVLTreeNode(data);
     }else if(data < root->val){
-        root->left = insert(root->left, data);
+        root->left = Insert(root->left, data);
         if(getHeight(root->left) - getHeight(root->right) == 2){
             if(data < root->left->val)
                 root = LLRotation(root);
@@ -52,7 +52,7 @@ AVLTreeNode* insert(AVLTreeNode* &root, int data){
                 root = LRRotation(root);
         }
     }else if(data > root->val){
-        root->right = insert(root->right, data);
+        root->right = Insert(root->right, data);
         if(getHeight(root->right) - getHeight(root->left) == 2){
             if(data > root->right->val)
                 root = RRRotation(root);
@@ -62,37 +62,37 @@ AVLTreeNode* insert(AVLTreeNode* &root, int data){
     }else{
         cout << "fuck" << endl;
     }
-    root->height = max(root->left->height, root->right->height) + 1;
+    root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
     return root;
 }
 
 AVLTreeNode* findMax(AVLTreeNode* root){
-    if(t == nullptr)
+    if(root == nullptr)
         return nullptr;
-    if(t->right == nullptr)
-        return t;
-    return findMax(t->right);
+    if(root->right == nullptr)
+        return root;
+    return findMax(root->right);
 }
 
 AVLTreeNode* findMin(AVLTreeNode* root){
-    if(t == nullptr)
+    if(root == nullptr)
         return nullptr;
-    if(t->left == nullptr)
-        return t;
-    return findMin(t->left);
+    if(root->left == nullptr)
+        return root;
+    return findMin(root->left);
 }
 
-bool delete(AVLTreeNode* &root, int x){
+bool Delete(AVLTreeNode* &root, int x){
     if(root == nullptr)
         return false;
     else if(root->val == x){
         if(root->left != nullptr && root->right != nullptr){
             if(getHeight(root->left) > getHeight(root->right)){
                 root->val = findMax(root->left)->val;
-                delete(root->left, root->val);
+                Delete(root->left, root->val);
             }else{
                 root->val = findMin(root->right)->val;
-                delete(root->right, root->val);
+                Delete(root->right, root->val);
             }
         }else{
             AVLTreeNode *old = root;
@@ -100,7 +100,7 @@ bool delete(AVLTreeNode* &root, int x){
             delete old;
         }
     }else if(x < root->val){
-        delete(root->left, x);
+        Delete(root->left, x);
         if(getHeight(root->right) - getHeight(root->left) == 2){
             if(getHeight(root->right->left) > getHeight(root->right->right))
                 root = RLRotation(root);
@@ -109,7 +109,7 @@ bool delete(AVLTreeNode* &root, int x){
         }else
             root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
     }else{
-        delete(root->right, x);
+        Delete(root->right, x);
         if(getHeight(root->left) - getHeight(root->right) == 2){
             if(getHeight(root->left->right) > getHeight(root->left->left))
                 root = LRRotation(root);
@@ -121,8 +121,24 @@ bool delete(AVLTreeNode* &root, int x){
     return true;
 }
 
+AVLTreeNode* findNode(AVLTreeNode* root, int x){
+    if(root == nullptr || root->val == x)
+        return root;
+    else if(x < root->val)
+        return findNode(root->left, x);
+    else
+        return findNode(root->right, x);
+}
+
 int main()
 {
-    
+    int N, tmp;
+    cin >> N;
+    AVLTreeNode *root = nullptr;
+    while(N--){
+        cin >> tmp;
+        root = Insert(root, tmp);
+    }
+    cout << root->val;
     return 0;
 }
